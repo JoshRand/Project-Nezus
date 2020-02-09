83d7d3f694326f80a6187e7d2e68fd1e0dc0c2e5
Joshua anderson <Mp4ct@live.com>
2020-02-08 18:12:58 -0600

    Added textures to the batch renderer

--- Nezus/src/graphics/batchrenderer2d.cpp
+++ Nezus/src/graphics/batchrenderer2d.cpp
@@ -22,10 +22,15 @@ namespace nezus {
 			glBindVertexArray(m_VAO);
 			glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 			glBufferData(GL_ARRAY_BUFFER, RENDERER_BUFFER_SIZE, NULL, GL_DYNAMIC_DRAW);
+
 			glEnableVertexAttribArray(SHADER_VERTEX_INDEX);
+			glEnableVertexAttribArray(SHADER_UV_INDEX);
 			glEnableVertexAttribArray(SHADER_COLOR_INDEX);
+
 			glVertexAttribPointer(SHADER_VERTEX_INDEX, 3, GL_FLOAT, GL_FALSE, RENDERER_VERTEX_SIZE, (const GLvoid*)0);
+			glVertexAttribPointer(SHADER_UV_INDEX, 2, GL_FLOAT, GL_FALSE, RENDERER_VERTEX_SIZE, (const GLvoid*)(offsetof(VertexData, VertexData::uv)));
 			glVertexAttribPointer(SHADER_COLOR_INDEX, 4, GL_UNSIGNED_BYTE, GL_TRUE, RENDERER_VERTEX_SIZE, (const GLvoid*)(offsetof(VertexData, VertexData::color)));
+
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 			GLuint* indices = new GLuint[RENDERER_INDICES_SIZE];
@@ -60,7 +65,7 @@ namespace nezus {
 			const math::vec3& position = renderable->getPosition();
 			const math::vec2& size = renderable->getSize();
 			const math::vec4& color = renderable->getColor();
-
+			const std::vector<math::vec2>& uv = renderable->getUV();
 			int r = color.x * 255.0f;
 			int g = color.y * 255.0f;
 			int b = color.z * 255.0f;
@@ -69,18 +74,22 @@ namespace nezus {
 			unsigned int c = a << 24 | b << 16 | g << 8 | r;
 
 			m_Buffer->vertex = *m_TransformationBack * position;
+			m_Buffer->uv = uv[0];
 			m_Buffer->color = c;
 			m_Buffer++;
 
 			m_Buffer->vertex = *m_TransformationBack * math::vec3(position.x, position.y + size.y, position.z);
+			m_Buffer->uv = uv[1];
 			m_Buffer->color = c;
 			m_Buffer++;
 
 			m_Buffer->vertex = *m_TransformationBack * math::vec3(position.x + size.x, position.y + size.y, position.z);
+			m_Buffer->uv = uv[2];
 			m_Buffer->color = c;
 			m_Buffer++;
 
 			m_Buffer->vertex = *m_TransformationBack * math::vec3(position.x + size.x, position.y, position.z);
+			m_Buffer->uv = uv[3];
 			m_Buffer->color = c;
 			m_Buffer++;
 
--- Nezus/src/graphics/renderable2d.h
+++ Nezus/src/graphics/renderable2d.h
@@ -15,7 +15,7 @@ namespace nezus {
 		struct VertexData
 		{
 			math::vec3 vertex;
-			// maths::vec4 color;
+			math::vec2 uv;
 			unsigned int color;
 		};
 
@@ -25,12 +25,14 @@ namespace nezus {
 			math::vec3 m_Position;
 			math::vec2 m_Size;
 			math::vec4 m_Color;
+			std::vector<math::vec2> m_UV;
 		protected:
-			Renderable2D() { }
+			Renderable2D() 
+			{setUVDefaults();}
 		public:
 			Renderable2D(math::vec3 position, math::vec2 size, math::vec4 color)
 				: m_Position(position), m_Size(size), m_Color(color)
-			{ }
+			{ setUVDefaults();}
 
 			virtual ~Renderable2D() { }
 
@@ -42,6 +44,15 @@ namespace nezus {
 			inline const math::vec3& getPosition() const { return m_Position; }
 			inline const math::vec2& getSize() const { return m_Size; }
 			inline const math::vec4& getColor() const { return m_Color; }
+			inline const std::vector<math::vec2>& getUV() const { return m_UV; }
+		private:
+			void setUVDefaults()
+			{
+				m_UV.push_back(math::vec2(0, 0));
+				m_UV.push_back(math::vec2(0, 1));
+				m_UV.push_back(math::vec2(1, 1));
+				m_UV.push_back(math::vec2(1, 0));
+			}
 		};
 
 	}
